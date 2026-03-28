d37a22b971eafbd5a9d5498d1db296151e32b24b
Chris Pennington <cpenn368@gmail.com>
2026-03-28 16:28:11 -0600

    scaffold: SPM project with stub app entry

---
+++ Sources/ClaudeTamagotchi/ClaudeTamagotchiApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ClaudeTamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("ClaudeTamagotchi")
+                .frame(width: 220, height: 260)
+        }
+    }
+}
